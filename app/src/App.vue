45034d89a50ea6d0db8d4b1e1e72528a9e9d008d
Tony Storti <tonystorti@gmail.com>
2017-09-06 09:28:55 -0500

    changed file structure to webpack template

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="app">
-    <h1>{{ msg }}</h1>
+    <img src="./assets/logo.png">
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Hello Vue!'
-    }
-  }
+  name: 'app'
 }
 </script>
 
 <style>
-
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
